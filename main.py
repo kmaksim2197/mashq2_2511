a2794d90e1ab339440595103eec377ef35cefcb7
kmaksim2197 <kmaksim2197@gmail.com>
2025-11-25 14:38:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-s = input()
-words = s.split()
-code = ""
-for w in words:
-    code += w[0]
-print(code)
+words = ["dasturlash", "kitob", "shunday", "kompyuter", "ilm", "maktab"]
+uzunliklar = [len(word) for word in words]
+print("1-chi eng uzun so'z:", words[uzunliklar.index(max(uzunliklar))])
+print("2-chi eng uzun so'z:", sorted(words, key=len, reverse=True)[1])
+
+s = input("Biror matn kiriting (so'z yoki matn): ")
+harf = input("Qaysi harfni hisoblaymiz? ")
+print(f"'{harf}' harfi matnda {s.lower().count(harf.lower())} marta uchraydi")
+
+fio = input("F.I.O. kiriting: ").strip()
+print("Faqat birinchi va oxirgi harflar:", fio[0] + fio[-1])
+print("O'rtadagi barcha harflar o'rniga X:", fio[0] + "X"*(len(fio)-2) + fio[-1] if len(fio)>=2 else fio)
+print("Agar ism uzunligi 2 yoki undan kam bo'lsa, uni o'zgartirmasdan chiqarish:")
+print(fio if len(fio) <= 2 else fio)
